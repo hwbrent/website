cda33173cfa707e5020640cb4dc8314582becb5c
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-19 21:28:04 +0100

    Call function whenever date object updates

--- src/components/menu-bar/DateAndTime.js
+++ src/components/menu-bar/DateAndTime.js
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 /**
  * @summary Gets a new `Date` object
  * @returns {Date}
  */
 const getDate = () => new Date();
+
+/**
+ * @summary Custom hook to execute `callback` whenever the time changes
+ * @param {Function} callback - a function to be called when the date or time changes
+ * @param {Date} dateObj - the value to be monitored
+ * @returns {void}
+ */
+const onDateTimeChange = (callback, dateObj) => useEffect(callback, [dateObj]);
 
 /**
  * @summary Represents the widget on the far right of the menu bar which shows the
@@ -19,6 +27,12 @@ export default function DateAndTime() {
     // update the tracked date/time every second
     const updateDate = () => setDate(getDate());
     setInterval(updateDate, 1000);
+
+    // when the date object updates, update the string representation of it.
+    const formatDate = () => {
+        console.log('formatDate');
+    };
+    onDateTimeChange(formatDate, date);
 
     console.log(date);
 
