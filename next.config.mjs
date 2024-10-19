ed80476acf3ba9a9f2f35eae254227d3624b5452
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-19 19:43:34 +0100

    Amend config
    See https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/#heading-step-2-configure-the-nextjs-build-process

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // GitHub Pages is designed to host static files, which means we can publish only
+    // HTML, CSS, JavaScript (and other static files) there. So we'll need to enable
+    // static page generation in Next.js.
+    // See: https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/#heading-step-2-configure-the-nextjs-build-process
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 
 export default nextConfig;
