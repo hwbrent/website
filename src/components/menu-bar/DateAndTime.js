c49df6d7f4980762c6b445fb53671080527b22b5
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-19 21:09:33 +0100

    Store date as state

--- src/components/menu-bar/DateAndTime.js
+++ src/components/menu-bar/DateAndTime.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export default function DateAndTime() {
+
+    const [ date, setDate ] = useState(new Date());
+
+    console.log(date);
+
     return (
         <div className="date-and-time"></div>
     );
