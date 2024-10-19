7929678a662bec8a2e675ce144981e4508c66526
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-20 00:21:34 +0100

    Add empty Application Menu component

---
+++ src/components/menu-bar/ApplicationMenu.js
@@ -0,0 +1,9 @@
+/**
+ * @summary The left side of the menu bar, with the buttons like 'File', 'Edit', etc.
+ * @description In macOS it's specific to the active application. It includes various
+ * menus, which change based on the currently active app.
+ * @returns {React.JSX}
+ */
+export default function ApplicationMenu() {
+    return <></>;
+}
