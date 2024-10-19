1398ead81fe448b0a305b0ce21ec6c23863986af
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-19 21:05:27 +0100

    Add empty date and time component of menu bar

---
+++ src/components/menu-bar/DateAndTime.js
@@ -0,0 +1,5 @@
+export default function DateAndTime() {
+    return (
+        <div className="date-and-time"></div>
+    );
+};
--- src/components/menu-bar/MenuBar.js
+++ src/components/menu-bar/MenuBar.js
@@ -1,3 +1,9 @@
+import DateAndTime from './DateAndTime';
+
 export default function MenuBar() {
-    return <div className="menu-bar"></div>;
+    return (
+        <div className="menu-bar">
+            <DateAndTime />
+        </div>
+    );
 };
