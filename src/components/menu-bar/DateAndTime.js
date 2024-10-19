0083110e2da593400951b6101463da0f3dd28e49
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-19 21:16:45 +0100

    Update `date` every second

--- src/components/menu-bar/DateAndTime.js
+++ src/components/menu-bar/DateAndTime.js
@@ -1,8 +1,14 @@
 import { useState } from "react";
 
+const getDate = () => new Date();
+
 export default function DateAndTime() {
 
-    const [ date, setDate ] = useState(new Date());
+    const [ date, setDate ] = useState(getDate());
+
+    // update the tracked date/time every second
+    const updateDate = () => setDate(getDate());
+    setInterval(updateDate, 1000);
 
     console.log(date);
 
