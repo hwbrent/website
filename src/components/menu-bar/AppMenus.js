7e45a3d43f51dc8ff2ef68b58e919f7c0ecc50a6
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-20 00:31:57 +0100

    Add example list of menus

--- src/components/menu-bar/AppMenus.js
+++ src/components/menu-bar/AppMenus.js
@@ -1,3 +1,17 @@
+// the various menu items that you see when VSCode is open
+const menus = [
+    'Code',
+    'File',
+    'Edit',
+    'Selection',
+    'View',
+    'Go',
+    'Run',
+    'Terminal',
+    'Window',
+    'Help',
+];
+
 /**
  * @summary The left side of the menu bar, with the buttons like 'File', 'Edit', etc.
  * @description In macOS it's specific to the active application. It includes various
