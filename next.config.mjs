60fb968df92255ab1c3a467dd94d909560a3771b
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-19 19:48:02 +0100

    Add base path to config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
     // See: https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/#heading-step-2-configure-the-nextjs-build-process
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+
+    // Github assigns a dedicated subdomain after my username. But this project is
+    // published under the sub-path 'website'. Unfortunately, this will lead to issues
+    // with missing images and styles.
+    // By default, Next.js maps all static assets the domain. This means that the
+    // favicon.ico file will be resolved to hwbrent.github.io/favicon.ico instead of
+    // hwbrent.github.io/website/favicon.icon.
+    // To fix this, we can set up a path prefix by adding basePath, like so:
+    basePath: "/website"
 };
 
 export default nextConfig;
