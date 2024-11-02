79cb5f78e6ccdda55ac396bd91f32f6a227f6bc5
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-11-02 15:22:10 +0000

    Use next/image Image component
    Isn't finding images for some reason

--- src/components/dock/Dock.js
+++ src/components/dock/Dock.js
@@ -11,18 +11,16 @@ const iconNames = [
 export default function Dock() {
     const icons = iconNames.map((name, key) => {
 
-        // const path = `${ICONS_BASE_PATH}/${name}.icns`;
-        // const width = 16;
-        // const height = 16;
-        // return <Image
-        //     src={path}
-        //     width={width}
-        //     height={height}
-        //     alt={name}
-        //     key={key}
-        // />;
-
-        return <div key={key}>{name}</div>
+        const path = `${ICONS_BASE_PATH}/${name}.icns`;
+        const width = 16;
+        const height = 16;
+        return <Image
+            src={path}
+            width={width}
+            height={height}
+            alt={name}
+            key={key}
+        />;
     });
 
     return (
