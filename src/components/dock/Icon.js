5a45908dea6e8f5dd87d758e8698acfad8dcbd82
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-11-02 19:53:24 +0000

    Place the tooltip properly

--- src/components/dock/Icon.js
+++ src/components/dock/Icon.js
@@ -29,9 +29,11 @@ function initTooltip() {
 
 /**
  * @summary Shows the tooltip above the icon
+ * @description Puts the tooltip above the icon with the centres lined up
+ * @param {MouseEvent} event - The 'mouseenter' event
  * @param {string} iconName - The name of the icon, e.g. 'Launchpad'
  */
-function showTooltip(iconName) {
+function showTooltip(event, iconName) {
     if (!tooltip) {
         initTooltip();
     }
@@ -42,6 +44,34 @@ function showTooltip(iconName) {
 
     // Make the tooltip visible (this is the default 'display' value if you never set it)
     tooltip.style.display = '';
+
+    //////////////////////////////////////////////
+    /// Place the tooltip above the given icon ///
+    //////////////////////////////////////////////
+    // Basically, we get the position of the top edge of the icon. From that we get
+    // the bottom edge of the tooltip. Then we calculate the top left of the tooltip
+    // based off the widths and heights and stuff
+
+    const icon = event.currentTarget;
+    const iconRect = icon.getBoundingClientRect();
+
+    // Calculate the centre point of the top edge of the icon
+    const iconTopCentreX = iconRect.left + (iconRect.width / 2);
+    const iconTopCentreY = iconRect.top;
+
+    const distance = 20; // pixels - gap between icon and tooltip
+
+    // The centre point of the bottom edge of the tooltip
+    const tooltipBottomCentreY = iconTopCentreY - distance;
+
+    // Figure out the coordinates of the top left corner of the tooltip
+    const tooltipRect = tooltip.getBoundingClientRect();
+    const tooltipTopLeftY = tooltipBottomCentreY - tooltipRect.height;
+    const tooltipTopLeftX = iconTopCentreX - (tooltipRect.width / 2);
+
+    // Set the x and y coordinate values
+    tooltip.style.left = `${tooltipTopLeftX}px`;
+    tooltip.style.top = `${tooltipTopLeftY}px`;
 }
 
 /**
@@ -65,8 +95,8 @@ export default function Icon(png, key) {
     // Get the name of the file from the src
     const name = src.split('/').pop().split('.')[0];
 
-    const show = () => showTooltip(name);
-    const hide = () => hideTooltip(name);
+    const show = (event) => showTooltip(event, name);
+    const hide = (event) => hideTooltip(event, name);
 
     return (
         <div
