84a3a97b80c3384820fbd6f3014dcd222512ed02
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-11-02 12:56:11 +0000

    Render very simple icons

--- src/components/dock/Dock.js
+++ src/components/dock/Dock.js
@@ -1,5 +1,33 @@
+import Image from 'next/image';
+
+const ICONS_BASE_PATH = '../../../public/dock-icons';
+
+const iconNames = [
+    'Downloads',
+    'Finder',
+    'Launchpad'
+];
+
 export default function Dock() {
+    const icons = iconNames.map((name, key) => {
+
+        // const path = `${ICONS_BASE_PATH}/${name}.icns`;
+        // const width = 16;
+        // const height = 16;
+        // return <Image
+        //     src={path}
+        //     width={width}
+        //     height={height}
+        //     alt={name}
+        //     key={key}
+        // />;
+
+        return <div key={key}>{name}</div>
+    });
+
     return (
-        <div className="dock"></div>
+        <div className="dock">
+            {icons}
+        </div>
     );
 }
