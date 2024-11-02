4d4333773e10c96119243e5d47df9f7c6fdae273
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-11-02 20:27:15 +0000

    Add link to icons

--- src/components/dock/Dock.js
+++ src/components/dock/Dock.js
@@ -1,3 +1,6 @@
+// Source: https://macosicons.com/#/
+// Most of them came in as .icns files, which apparently are native to MacOS and not
+// able to be (easily) loaded. So I converted them to .png format
 import DownloadsPNG from 'public/dock-icons/Downloads.png';
 import FinderPNG    from 'public/dock-icons/Finder.png';
 import LaunchpadPNG from 'public/dock-icons/Launchpad.png';
