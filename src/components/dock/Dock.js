f62d7cd907168e6a3bc7aa238e08f24a9c4dff3b
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-11-02 18:06:49 +0000

    Clean up spacing

--- src/components/dock/Dock.js
+++ src/components/dock/Dock.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 import DownloadsPNG from 'public/dock-icons/Downloads.png';
-import FinderPNG from 'public/dock-icons/Finder.png';
+import FinderPNG    from 'public/dock-icons/Finder.png';
 import LaunchpadPNG from 'public/dock-icons/Launchpad.png';
 
 const ICONS = [
@@ -12,11 +12,11 @@ const ICONS = [
 
 const getIcons = () => ICONS.map((icon, key) => (
     <Image
-        src={icon.src}
-        width={16}
-        height={16}
-        alt={''}
-        key={key}
+        src    = {icon.src}
+        width  = {16}
+        height = {16}
+        alt    = {''}
+        key    = {key}
     />
 ));
 
