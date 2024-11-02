3e0325ac89a1f1c8929a1770c2783a5b99ac16ff
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-11-02 19:22:31 +0000

    Add functions to show/hide tooltip above icon

--- src/components/dock/Icon.js
+++ src/components/dock/Icon.js
@@ -4,6 +4,44 @@ import Image from 'next/image';
 const SIZE = 52;
 
 /**
+ * @summary The tooltip thing that appears when you hover over an {@link Icon}
+ * @type {HTMLDivElement}
+ */
+let tooltip;
+
+/**
+ * @summary Creates the tooltip element
+ */
+function initTooltip() {
+    tooltip = document.createElement('div');
+
+    // add it to the DOM
+    const root = document.getElementById('root');
+    root.appendChild(tooltip);
+
+    // Add a class to allow styling
+    tooltip.classList.add('tooltip');
+
+    // add a text element within it
+    const p = document.createElement('p');
+    tooltip.appendChild(p);
+}
+
+/**
+ * @summary Shows the tooltip above the icon
+ */
+function showTooltip() {
+    if (!tooltip) {
+        initTooltip();
+    }
+}
+
+/**
+ * @summary Hides the tooltip
+ */
+function hideTooltip() {}
+
+/**
  * @summary An icon in the macOS dock
  * @param {object} png - an object containing data relating to this icon and its image
  * @param {number} key - the index of this icon in the overall ICONS array
@@ -17,7 +55,12 @@ export default function Icon(png, key) {
     const name = src.split('/').pop().split('.')[0];
 
     return (
-        <div className='icon' key={key}>
+        <div
+            className    = 'icon'
+            key          = {key}
+            onMouseEnter = {showTooltip}
+            onMouseLeave = {hideTooltip}
+        >
             <Image
                 src    = {src}
                 width  = {SIZE}
