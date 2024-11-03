533bd3ef33a55b86c2dd050858c3f69069762f51
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-11-03 13:44:02 +0000

    Add file for popup-related stuff

---
+++ src/components/menu-bar/Popup.js
@@ -0,0 +1,15 @@
+/**
+ * @summary The little window that appears under a menu when it's highlighted
+ * @type {HTMLDivElement}
+ */
+let popup;
+
+/**
+ * @summary Shows the popup under the given menu
+ */
+export function showPopup(wrapperRef) {}
+
+/**
+ * @summary Hides the popup
+ */
+export function hidePopup() {}
