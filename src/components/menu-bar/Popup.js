be57a8ff7ad5f1508af848e35a4d78f2d576ada4
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-11-03 13:45:48 +0000

    Add logic to init popup

--- src/components/menu-bar/Popup.js
+++ src/components/menu-bar/Popup.js
@@ -5,9 +5,27 @@
 let popup;
 
 /**
+ * @summary Creates the popup element
+ */
+function initPopup() {
+    popup = document.createElement('div');
+
+    // add it to the DOM
+    const root = document.getElementById('root');
+    root.appendChild(popup);
+
+    // Add a class to allow styling
+    popup.classList.add('popup');
+}
+
+/**
  * @summary Shows the popup under the given menu
  */
-export function showPopup(wrapperRef) {}
+export function showPopup(wrapperRef) {
+    if (!popup) {
+        initPopup();
+    }
+}
 
 /**
  * @summary Hides the popup
